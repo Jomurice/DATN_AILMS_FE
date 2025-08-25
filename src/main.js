1ffcaf5610d1c4138fa2c38bfe4867d89ad38855
dung02 <ledung180518@gmail.com>
2025-08-25 13:41:09 +0700

    c

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 // Import bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 createApp(App).use(router).mount('#app')
