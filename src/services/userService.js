93231659c971e7071017e0f0c2d967ac8e8881cc
Quang <minhquangru@gmail.com>
2025-08-25 15:58:50 +0700

    fix api

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,12 +3,12 @@ import api from "./axios";
 export const userService = {
 
     async getAllUsers(){
-        const response = await api.get("/users");
+        const response = await api.get("/api/users");
         return response.data.result;
     },
 
     async getUserById(userId){
-        const response = await api.get(`/users/${userId}`);
+        const response = await api.get(`/api/users/${userId}`);
         return response.data.result;
     }
 };
