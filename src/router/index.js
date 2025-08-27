460f6b8795cd1dbcac616040e490ca174d7076d2
dung02 <ledung180518@gmail.com>
2025-08-27 10:25:49 +0700

    update

--- src/router/index.js
+++ src/router/index.js
@@ -9,43 +9,48 @@ import Zone from "../components/warehouseManagement/Zone.vue";
 import Aisle from "../components/warehouseManagement/Aisle.vue";    
 
 const routes = [
-  { path: "/", name: "home", component: HomeView },
-  { path: "/profile", name: "profile", component: ProfileView, meta: { title: "Profile" } },
+    {path: "/", name: "home", component: HomeView},
+    {path: "/profile", name: "profile", component: ProfileView,meta: { title: 'Profile' }},
+    {path: "/warehouse",
+        children:[
+            {path:'/admin/warehouse'}
+        ],
+        component: Warehouse, meta: { title: 'Warehouse' }},
+    {path: '/warehouse/:id/zone', component: Zone, meta: { title: 'Zone' },props: true },
+    {path: '/warehouse/:id/zone/:id/aisle', component: Aisle, meta: { title: 'Aisle' },props: true },
 
-  // Warehouse - user có thể xem
-  { path: "/warehouse", component: Warehouse, meta: { title: "Warehouse" } },
-  { path: "/warehouse/:warehouseId/zone", component: Zone, meta: { title: "Zone" }, props: true },
-  { path: "/warehouse/:warehouseId/zone/:zoneId/aisle", component: Aisle, meta: { title: "Aisle" }, props: true },
 
-  // Admin - chỉ admin mới vào được
-  {
-    path: "/admin",
-    meta: { title: "Admin" },
+
+    {path:'/admin',
+    meta: { title: 'Admin' },
     children: [
-      {
-        path: "account",
-        component: AccountManagerment,
-        meta: { title: "Account" },
-      },
-      {
-        path: "account/:id/detail",
-        component: AccountDetail,
-        meta: { title: "Account Detail" },
-        props: true,
-      },
-      {
-        path: "warehouse/:id/detail",
-        component: WarehouseDetail,
-        meta: { title: "Warehouse Detail" },
-        props: true,
-      },
-    ],
-  },
-];
+        {
+            path: 'account', 
+            component: AccountManagerment,
+            meta: { title: 'Account' }
+        },
+        {
+            path:'account/:id/detail',
+            component: AccountDetail,
+            meta: {title: 'Detail'}
+        },
+        {
+            path: 'warehouse/detail',
+            children: [
+               { path: 'warehouse/:id/detail'}
+            ],
+            component: WarehouseDetail,
+            meta: {title: 'Detail'}
+        },
+    ]
+    }
+]
+
+
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
