ec3830f69b733e4dea776073d85666f6d8dd8297
Quang <minhquangru@gmail.com>
2025-09-26 15:56:11 +0700

    fix: name header

--- src/components/layout/TopNav.vue
+++ src/components/layout/TopNav.vue
@@ -10,11 +10,11 @@
           <RouterLink class="tab" to="/inbound" exact-active-class="active">Nhập hàng</RouterLink>
           <RouterLink class="tab" to="/outbound/new" exact-active-class="active">Đơn xuất</RouterLink>
           <RouterLink class="tab" to="/outbound" exact-active-class="active">Xuất hàng</RouterLink>
-          <RouterLink class="tab" to="/warehouse" exact-active-class="active">Kho</RouterLink>
+          <RouterLink class="tab" to="/warehousesmenu" exact-active-class="active">Kho</RouterLink>
           <RouterLink class="tab" to="/report" exact-active-class="active">Báo cáo</RouterLink>
           <RouterLink class="tab" to="/admin/account" exact-active-class="active">Nhân sự</RouterLink>
           <RouterLink class="tab" to="/location" exact-active-class="active">Địa chỉ</RouterLink>
-          <RouterLink class="tab" to="/warehousesmenu">Menu</RouterLink>
+          
           <!-- <RouterLink class="tab" to="/report" exact-active-class="active">Tổng quát</RouterLink>
           <RouterLink class="tab" to="/report" exact-active-class="active">Khác</RouterLink> -->
         </div>
